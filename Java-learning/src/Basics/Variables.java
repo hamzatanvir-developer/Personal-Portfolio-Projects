e681f5df53ba8cacf093caf93996982be2558376
Muhammad Hamza <hamzatanvir501@gmail.com>
2026-05-09 02:16:11 +0500

    Variables and Data types with byte sizes

--- Java-learning/src/Basics/Variables.java
+++ Java-learning/src/Basics/Variables.java
@@ -13,6 +13,8 @@ public class Variables {
         short                       2
         boolean                     1*/
 
+        //Primitive data types
+
         // Integer types
         byte age = 19;           // 1 byte
         short year = 2024;       // 2 bytes
@@ -20,13 +22,21 @@ public class Variables {
         long population = 8000000000L;  // 8 bytes
 
         // Decimal types
-        float height = 5.9f;     // 4 bytes
+        float height = 5.9F;     // 4 bytes
         double gpa = 3.5;       // 8 bytes
 
         // Other types
         boolean isStudent = true;  // 1 byte
         char grade = 'A';          // 2 bytes (Java uses UTF-16)
         String name = "Muhammad Hamza";     // varies
+
+
+        //Non-Primitive data types
+
+        //String
+        String place = "Lahore and Multan";
+        System.out.println(place.length());
+        System.out.println(place.substring(0,7));
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
