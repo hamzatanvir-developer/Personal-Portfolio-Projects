1b70dbd200b3084f9ee91ee40b64924cf4ccf823
Muhammad Hamza <hamzatanvir501@gmail.com>
2026-05-09 03:21:07 +0500

    Variables and Data types with byte sizes

---
+++ Java-learning/src/Basics/Arrays.java
@@ -0,0 +1,30 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //Array creation nd initialization
+        //Input
+        Scanner input = new Scanner(System.in);
+
+        //Input of size
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+        int[] marks = new int[size];
+
+        //Input of numbers in array
+        System.out.print("Enter marks: ");
+        for (int i = 0; i< size; i++){
+            marks[i] = input.nextInt();
+        }
+
+        //Output
+        System.out.println("You Entered: ");
+        for (int i = 0; i < size; i++){
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
