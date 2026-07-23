e2cf77828dc25c665e95e9311938bc103ab3f95f
Muhammad Hamza <hamzatanvir501@gmail.com>
2026-07-24 00:04:16 +0500

    Personal Monthly Budget Planner project

---
+++ Java-learning/src/Basics/PersonalBudgtPlanner.java
@@ -0,0 +1,23 @@
+
+/*Required Features
+    --> Enter monthly income
+    --> Store multiple expenses in an array
+    --> Calculate total expenses
+    --> Calculate remaining balance
+    --> Show warning when expenses exceed income
+
+Concepts Used
+
+    Arrays
+    Loops
+    Scanner
+    Methods
+    Math operators
+    Strings
+    Exception Handling
+    if/else*/
+
+package Basics;
+
+public class PersonalBudgtPlanner {
+}
